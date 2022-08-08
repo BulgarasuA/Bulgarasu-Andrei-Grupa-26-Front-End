e9d19177d8f3eb2f8ac2214d9c6f15e1d08639d8
Bulgarasu Andrei <Bulgarasu_andrei89@yahoo.com>
2022-08-08 18:37:20 +0300

    add tools

--- SPA/my-first-app/pages/getHomePage.js
+++ SPA/my-first-app/pages/getHomePage.js
@@ -1,11 +1,17 @@
+import dayjs from "dayjs";
 import "./home.css";
 
+
+const today = dayjs().format('DD/MM/YYYY');
 const getHomePage = () => {
     return `
     <h1 class = "home-title">Home Page</h1>
+    <p>${today}</p>
     <p class = 'paragraph-home'>lorem ipsum dolor sit amet consectetur adipisicing el. </p>
     <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
     `
 }
+
+
 
 export default getHomePage;
