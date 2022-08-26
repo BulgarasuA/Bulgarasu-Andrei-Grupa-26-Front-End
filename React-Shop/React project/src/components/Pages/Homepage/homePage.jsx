bee43f46ee84df96bfba0432bba33705c390a3eb
Bulgarasu Andrei <Bulgarasu_andrei89@yahoo.com>
2022-08-26 13:31:49 +0300

    react shop 1h 8min

--- React-Shop/React project/src/components/Pages/Homepage/homePage.jsx
+++ React-Shop/React project/src/components/Pages/Homepage/homePage.jsx
@@ -1,12 +1,27 @@
 
 import "./homePage.css";
+import ProductList from './produse.json';
 
 function HomePage() {
-
+    console.log(ProductList);
     return (
         <>
             <h1 className='home-title'>Home Page</h1>
-
+            <div className="product-list">
+                <div className="product">
+                    {ProductList.map((product) => {
+                        return (
+                            <div className='products'>
+                                <p>{product.title}</p>
+                                <img src={product.image} alt="" />
+                                <p>{product.price}</p>
+                                <p>"{product.description}"</p>
+                                <button>Add to card</button>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
         </>
     )
 }
